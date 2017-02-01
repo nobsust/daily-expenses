3dc93ab259044d0776901af3d5ae4b23f2669aa4
Sam <sjy@exilesoft.local>
2017-02-01 12:50:00 +0530

    heroku stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -jar target/daily-expenses-1.0-SNAPSHOT-fat.jar
+web: java -jar target/daily-expenses-1.0-SNAPSHOT-fat.jar
--- src/main/java/com/github/nobsust/dex/Server.java
+++ src/main/java/com/github/nobsust/dex/Server.java
@@ -7,11 +7,13 @@ import io.vertx.core.AbstractVerticle;
  */
 public class Server extends AbstractVerticle {
 
+    private final int PORT = Integer.valueOf(System.getenv("PORT"));
+
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
             req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>");
-        }).listen(8080);
+        }).listen(PORT);
     }
 
     @Override
