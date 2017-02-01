c8d6794f1c9d9572f1d273da12a10e12dd7afbb6
Sam <sjy@exilesoft.local>
2017-02-01 14:17:21 +0530

    port fixed

--- src/main/java/com/github/nobsust/dex/Server.java
+++ src/main/java/com/github/nobsust/dex/Server.java
@@ -1,19 +1,34 @@
 package com.github.nobsust.dex;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 /**
  * Created by Sam on 2/1/2017.
  */
 public class Server extends AbstractVerticle {
 
-    private final int PORT = Integer.valueOf(System.getenv("PORT"));
+    private final int HTTP_PORT = Integer.valueOf(System.getenv("PORT"));
 
     @Override
-    public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>");
-        }).listen(PORT);
+    public void start(Future<Void> future) throws Exception {
+
+        Router router = Router.router(vertx);
+
+        router.route("/*").handler(StaticHandler.create("public"));
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(HTTP_PORT, result -> {
+                    if (result.succeeded()) {
+                        future.complete();
+                    } else {
+                        future.fail(result.cause());
+                    }
+                });
     }
 
     @Override
